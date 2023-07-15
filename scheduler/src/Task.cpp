c0b2647b432bd7880a07292bece2c7ad4e06a489
AndyKong <andy.kong@nuaa.edu.cn>
2023-07-15 19:57:45 +0800

    [feat]添加task类

--- scheduler/src/Task.cpp
+++ scheduler/src/Task.cpp
@@ -13,3 +13,28 @@ int Task::getTaskId() const {
     return task_id;
 }
 
+void Task::runTask() {
+
+}
+
+void RouteTask::addToRouteList(const Eigen::Matrix<double, 3, 2> & route_point)
+{
+    route_list.push_back(route_point);
+}
+
+Eigen::Matrix<double, 3, 2> RouteTask::nextRoutePoint()
+{
+    route_index++;
+    return route_list[route_index];
+}
+
+Eigen::Matrix<double, 3, 2> RouteTask::setCurrentRoutePoint(const Eigen::Matrix<double, 3, 2> & self_pose)
+{
+
+}
+
+int RouteTask::getCurrentRouteIndex()
+{
+    return route_index;
+}
+
--- scheduler/src/TaskManager.cpp
+++ scheduler/src/TaskManager.cpp
@@ -6,19 +6,7 @@
 
 TaskManager::TaskManager()
 {
-    route_index = 0;
     task_index = 0;
-}
-
-void TaskManager::addToRouteList(const Eigen::Matrix<double, 3, 2> & route_point)
-{
-    route_list.push_back(route_point);
-}
-
-Eigen::Matrix<double, 3, 2> TaskManager::nextRoutePoint()
-{
-    route_index++;
-    return route_list[route_index];
 }
 
 void TaskManager::nextTask()
@@ -26,24 +14,12 @@ void TaskManager::nextTask()
     task_index++;
 }
 
-Eigen::Matrix<double, 3, 2> TaskManager::setCurrentRoutePoint(const Eigen::Matrix<double, 3, 2> & self_pose)
-{
-
-}
-
-int TaskManager::getCurrentRouteIndex()
-{
-    return route_index;
-}
-
-int TaskManager::getCurrentTask()
+int TaskManager::getCurrentTask() const
 {
     return task_index;
 }
 
 void TaskManager::reset()
 {
-    route_index = 0;
     task_index = 0;
-    route_list.clear();
 }
